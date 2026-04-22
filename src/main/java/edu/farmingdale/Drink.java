36455e8a6a3f890719d2f86f9a59d1fb874f65cc
Tommy Jr <rabincrash@gmail.com>
2026-04-21 20:56:58 -0400

    start of project

---
+++ src/main/java/edu/farmingdale/Drink.java
@@ -0,0 +1,9 @@
+package edu.farmingdale;
+
+public interface Drink {
+
+    public Drink size(String str);
+    public Drink hotIced(String str);
+    public Drink withMilk(String str);
+    public Drink sweetner(String str);
+}
